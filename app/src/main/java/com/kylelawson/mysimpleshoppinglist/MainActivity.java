a29acbac57f63827d476d7aae90755a2d443254a
Kyle Lawson <kylelawson@mail4me.com>
2016-06-24 12:52:37 -0500

    Modified color scheme and fixed floating action button look

--- app/src/main/java/com/kylelawson/mysimpleshoppinglist/MainActivity.java
+++ app/src/main/java/com/kylelawson/mysimpleshoppinglist/MainActivity.java
@@ -1,12 +1,16 @@
 package com.kylelawson.mysimpleshoppinglist;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -29,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         //For the toolbar
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().show();
 
         //Listview
         shoppingListNameArray = ShoppingListItem.createShoppingItem(1);
@@ -53,6 +58,41 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //Adds the menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    //Adds actions to menu item selections
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.settings:
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(R.string.about)
+                        .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setItems(R.array.about_credit, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //This sets up the recycler view swipe
     private void setupItemTouchHelper(){
         ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
             @Override
